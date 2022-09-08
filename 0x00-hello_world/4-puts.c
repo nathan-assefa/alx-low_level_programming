944531a9311a4986497262da03b6e6792a988a66
nathan-assefa <nnathanassefa@gmail.com>
2022-09-08 07:12:46 -0700

    Fixing the comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main: an entry to c program
- * return 0: the program completed successfully
+ * main is an entry to c program
+ *
+ * return 0 when the program completed successfully
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
 }
