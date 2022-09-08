f4230d0e3295b32b132f640767a1a2cd93b5008e
nathan-assefa <nnathanassefa@gmail.com>
2022-09-08 06:38:34 -0700

    Using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * main: an entry to c program
+ * return 0: the program completed successfully
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
