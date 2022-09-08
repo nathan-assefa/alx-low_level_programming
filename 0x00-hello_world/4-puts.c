1a2af066af2a108071351ddf7183c245264b5467
nathan-assefa <nnathanassefa@gmail.com>
2022-09-08 06:57:44 -0700

    Add comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main: an entry to c program
  * return 0: the program completed successfully
-*/
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
