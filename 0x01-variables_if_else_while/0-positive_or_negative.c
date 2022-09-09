390600c468bb6c8f10b8e587330eec868ed7ecdf
nathan-assefa <nnathanassefa@gmail.com>
2022-09-09 01:45:00 -0700

    Conditions

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - printing
+ *
+ * Description: The function prints a text
+ * depending on the conditions
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX;
+
+	if (n > 0)
+	{
+		printf("%i is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero", n);
+	}
+	else
+	{
+		printf("%i is negative", n);
+	}
