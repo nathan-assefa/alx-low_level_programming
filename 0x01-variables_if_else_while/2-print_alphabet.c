ef17d1ba2dbb6d71b6e96d34c0b74a55a9037867
nathan-assefa <nnathanassefa@gmail.com>
2022-09-09 06:20:25 -0700

    Lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing
+ *
+ * Return: 0 when the program succeed
+ */
+int main(void)
+{
+char alphabet = 'a'
+
+while(alphabet <= 'z')
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+}
