902d980d37d1de209592360bd20a42c107bbb196
nathan-assefa <nnathanassefa@gmail.com>
2022-09-10 14:17:35 -0700

    Adding comment

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+nclude <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: this function prints 
+ * the sequance of numbers
+ * Return: 0 when the program is succeed
+ */
+int main(void)
+{
+	int n1 = 0, n2;
+
+	while (n1 <= 99)
+	{
+		n2 = n1;
+
+		while (n2 <= 99)
+		{
+			if (n2 != n1)
+			{
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+
+				if (n1 != 98 || n2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+
+			n2++;
+		}
+
+		n1++;
+	}
+
+	putchar('\n');
+	return(0);
+}
