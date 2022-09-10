bff6cf2b6021532fe1534c2fd50f67f0388fb9f8
nathan-assefa <nnathanassefa@gmail.com>
2022-09-10 04:52:16 -0700

    Adding comment

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: 0 when the progam is succeed
+ */
+int main(void)
+{
+    int num = 48;
+
+    while (num <= 57)
+    {
+        putchar(num);
+        if (num != 57)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        num++;
+    }
+
+    putchar('\n');
+    return (0);
+}
