fd777ed9c4d56aa3a76dd9d49c90f04de909dce6
nathan-assefa <nnathanassefa@gmail.com>
2022-09-10 00:30:56 -0700

    numbering

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing
+ *
+ * Return - 0
+ */
+int main(void)
+{
+int num = 0;
+
+while (num <= 9)
+{
+printf("%i", num);
+num++;
+}
+printf("\n");
+
+return (0);
+}
