4e532fcb7962260c300c12a9a2e5b84ac6f1c5ac
nathan-assefa <nnathanassefa@gmail.com>
2022-09-10 03:45:32 -0700

    Printing number using putchar function)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: 0 when the function is succeed
+ */
+int main(void)
+{
+int i = 48;
+
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+
+putchar('\n');
+return (0);
+}
