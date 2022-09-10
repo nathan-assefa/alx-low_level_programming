765d6da9be6fd9c7d5ea6801483d5dbf9888810f
nathan-assefa <nnathanassefa@gmail.com>
2022-09-10 04:32:42 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+nclude <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: 0 when the program is completed
+ */
+int main(void)
+{
+	char low = 'a';
+	int num = 48;
+
+	while (low <= 'f')
+	{
+
+		while (num <= 57)
+		{
+			putchar(num);
+			num++;
+		}
+
+		putchar(low);
+		low++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
