5dd246b387bba55fb725014294aec75ad439e8be
nathan-assefa <nnathanassefa@gmail.com>
2022-09-10 04:03:58 -0700

    Converting cases

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * mani - Entery point
+ *
+ * Return: 0 when the progam is successful
+ */
+int main(void)
+{
+char lower = a;
+
+while (lower <= 'z')
+{
+putchar(strrev(lower));
+lower++
+}
+
+putchar('\n');
+return (0);
+}
