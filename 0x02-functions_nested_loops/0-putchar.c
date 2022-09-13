f807df01e712e83c6e1995d2ad1d000bcf228737
nathan-assefa <nnathanassefa@gmail.com>
2022-09-13 05:02:02 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: always 0 when the program is completed
+ */
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+nclude "stdio.h"
+
+void print_alphabet(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int lower = 'a';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	putchar('\n');
+}
