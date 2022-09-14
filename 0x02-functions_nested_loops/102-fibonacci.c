404a5d87fcff044b6162b97a34cc3600fca90a7c
nathan-assefa <nnathanassefa@gmail.com>
2022-09-14 15:51:11 -0700

    98

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 1;
+	b = 2;
+
+	for (int i = 1; i < 50; i++)
+	{
+		printf("%d\n", a);
+		putchar(',');
+		putchar(' ');
+
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	putchar('\n');
+	return (0);
+}
