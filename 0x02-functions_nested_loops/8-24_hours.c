0b6c218577d090bcdad69f4bf71b136bc5159f51
nathan-assefa <nnathanassefa@gmail.com>
2022-09-14 04:56:43 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * jack_bauer: a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * Description: this function prints time
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int sec1, sec2, tim1, tim2;
+
+	tim1 = 0;
+	while (tim1 <= 2)
+	{
+		tim2 = 0;
+
+		while (tim2 <= 3)
+		{
+			sec1 = 0;
+
+			while (sec1 <= 5)
+			{
+				sec2 = 0;
+
+				while (sec2 <= 9)
+				{
+					_putchar(tim1 + '0');
+					_putchar(tim2 + '0');
+					_putchar(':');
+					_putchar(sec1 + '0');
+					_putchar(sec2 + '0');
+					_putchar('\n');
+					sec2++;
+				}
+
+				sec1++;
+			}
+
+			tim2++;
+		}
+
+		tim1++;
+	}
+	return (0);
+}
