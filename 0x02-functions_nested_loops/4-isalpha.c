eee9971f042f00536f5fd2d90b31035bf50ab92c
nathan-assefa <nnathanassefa@gmail.com>
2022-09-14 01:55:30 -0700

    Adding space

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha: a function that checks for alphabetic character
+ *
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return 0;
+}
