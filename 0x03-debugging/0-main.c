e4c0b77654317294cf64c3305f830c1d5c49ecea
nathan-assefa <nnathanassefa@gmail.com>
2022-09-15 13:00:44 -0700

    Checking for positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main- tets whether an integer is positive
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
