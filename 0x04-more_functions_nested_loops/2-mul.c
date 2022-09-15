b459cbe23961d6dbbd206b40e6bbd2cb8f199ac8
nathan-assefa <nnathanassefa@gmail.com>
2022-09-15 02:57:48 -0700

    Checking for digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul- a function that multiplies two integers.
+ *
+ * @a- argument
+ * @b- argument
+ *
+ * Return: always 0;
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
