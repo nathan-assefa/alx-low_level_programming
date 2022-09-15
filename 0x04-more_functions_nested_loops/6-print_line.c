e313d96c6d64fef3071bc31985f1cee82e970fa8
nathan-assefa <nnathanassefa@gmail.com>
2022-09-15 06:32:50 -0700

    Printing lin

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * print_line- print lines
+ *
+ * @n: argument
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 0; i <= n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
