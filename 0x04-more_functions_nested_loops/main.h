408791d952ed6144ef66cd81d7d054ddcef6ac86
nathan-assefa <nnathanassefa@gmail.com>
2022-09-15 04:42:20 -0700

    Condtion

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-#ifndef HOLBERTON_H_INCLUDED                                                                               
-
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H                                                                      
+#define MAIN_H
 
 int _isupper(int c);
 int _isdigit(int c);
