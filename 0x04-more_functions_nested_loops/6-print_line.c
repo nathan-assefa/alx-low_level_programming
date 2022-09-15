4a66ae0b3122dfdd7a2d16fc3d72d7d96e5526cc
nathan-assefa <nnathanassefa@gmail.com>
2022-09-15 07:53:21 -0700

    Adding lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,7 +7,14 @@
  */
 void print_line(int n)
 {
-	while (n-- < 0)
-		_putchar('_');
-	_putchar('n');
+	int i;
+
+	if (n <=0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar(95);
+		_putchar('\n');
+	}
 }
