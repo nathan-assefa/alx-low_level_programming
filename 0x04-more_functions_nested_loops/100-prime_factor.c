fe17da322512a72a92936ab946eb98054e2a44c8
nathan-assefa <nnathanassefa@gmail.com>
2022-09-18 14:02:25 +0300

    Removing trailiing spaces

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,34 +2,34 @@
 #include <stdio.h>
 
 /**
- * primefactors- to print prime factors 
- * 
- * @num: argument 
+ * primefactors- to print prime factors
+ *
+ * @num: argument
  */
-void primefactors(int num)  
-{  
-    int count;  
-  
-    printf("\nPrime Factors of %d are ..\n", num);  
-    for(count = 2; num > 1; count++)  
-    {  
-        while(num % count == 0)  
-        {  
-            printf("%d ", count);  
-            num = num / count;  
-        }  
-    }  
-    printf("\n");  
-}  
-  
-int main()  
-{  
-    int num;  
-  
-    printf("Enter a positive integer\n");  
-    scanf("%d", &num);  
-  
-    primefactors(num);  
-  
-    return 0;  
-}  
+void primefactors(int num)
+{
+    int count;
+
+    printf("\nPrime Factors of %d are ..\n", num);
+    for(count = 2; num > 1; count++)
+    {
+        while(num % count == 0)
+        {
+            printf("%d ", count);
+            num = num / count;
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a positive integer\n");
+    scanf("%d", &num);
+
+    primefactors(num);
+
+    return 0;
+}
