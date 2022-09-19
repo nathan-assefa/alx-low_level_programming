c9b398587c8866c8d32ab041d0c6caef6ca07a86
Nathan1-E <103236938+Nathan1-E@users.noreply.github.com>
2022-09-20 00:21:26 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,20 @@
  */
 void rev_string(char *s)
 {
-    int i;
+        char tmp;
+        int i, len1, len2;
 
-    for (i = 9; i >= 0; i--)
-    {
-        *s = *(s + i);
-    }
-    putchar('\n');
+        while (s[len1] != '\0')
+        {
+                len1++;
+        }
+
+        len2 = len1 - 1;
+        for (i = 0; i < len1 / 2; i++)
+        {
+                tmp = s[i];
+                s[i] = s[len2];
+                s[len2] = tmp;
+                len2 -= 1;
+        }
 }
