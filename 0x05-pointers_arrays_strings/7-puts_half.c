ecc2e52130594681ed7010689110b5b339ea4fc4
nathan-assefa <nnathanassefa@gmail.com>
2022-09-20 12:45:19 +0300

    Adding number

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,8 +11,14 @@ void puts_half(char *str)
     int i, j;
 
     j = strlen(str);
-    for (i = j / 2; i < j ; i++)
+    if (j % 2 == 0)
+        for (i = j / 2; i < j ; i++)
+        {
+            _putchar(str[i]);
+        }
+    else
     {
+        for (i = (j / 2) + 1; i < j; i++)
         _putchar(str[i]);
     }
     _putchar('\n');
