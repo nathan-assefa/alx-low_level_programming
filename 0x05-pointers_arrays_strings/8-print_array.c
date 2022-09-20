fd991d83ca66edcf9714481463c40dc8f156b563
nathan-assefa <nnathanassefa@gmail.com>
2022-09-20 13:25:52 +0300

    Printng array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,11 @@ void print_array(int *a, int n)
 {
     int i;
 
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < n; i++)
     {
         printf("%d", a[i]);
 
-        if (i < 4 )
+        if (i < n - 1 )
         {
             _putchar(',');
             _putchar(' ');
