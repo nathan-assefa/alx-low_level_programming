fe675f63391a2380a9f08fef3e7f3e5022f0ec77
nathan-assefa <nnathanassefa@gmail.com>
2022-09-20 12:04:48 +0300

    Removing -

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,9 @@ void puts2(char *str)
     int i, j;
 
     j = strlen(str);
-    for (i = 0; i < j - 1; i++)
+    for (i = 0; i < j; i++)
     {
         if (i % 2 == 0)
            _putchar(str[i]);
     }
-    if (i == 46)
-       _putchar('.');
-   _putchar('\n');
 }
