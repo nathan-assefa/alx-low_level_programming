3eeaf2064e09fd337e42db395ae44b5fb15d75ca
nathan-assefa <nnathanassefa@gmail.com>
2022-09-20 10:47:10 +0300

    Copying string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * rev_string- Revesing string
  *
  * @s: parameter
  */
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s[10] = "My School";
-
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
-}
-
 void rev_string(char *s)
 {
         char tmp;
@@ -34,7 +16,6 @@ void rev_string(char *s)
         {
                 len1++;
         }
-        printf("len1 is %d\n", len1);
 
         len2 = len1 - 1;
         for (i = 0; i < len1 / 2; i++)
