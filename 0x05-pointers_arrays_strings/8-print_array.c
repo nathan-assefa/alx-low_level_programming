68d4a7dc88d379d2b3e5174e0d378a548b28cbbe
nathan-assefa <nnathanassefa@gmail.com>
2022-09-20 13:36:53 +0300

    Printng array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,8 +17,7 @@ void print_array(int *a, int n)
 
         if (i < (n - 1) )
         {
-            _putchar(',');
-            _putchar(' ');
+            printf(", ");
         }
     }
     _putchar('\n');
