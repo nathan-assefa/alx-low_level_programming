6ed6d430fd5861eaa3c3c6062e387dd312e73ffb
nathan-assefa <nnathanassefa@gmail.com>
2022-09-21 14:01:58 -0700

    Fixing some issue

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat- concatinating string using bytes
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,6 +12,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
+	int n;
+	strcmp(s1, s2, n);
 	return (s1);
 }
