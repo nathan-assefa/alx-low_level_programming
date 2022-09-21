10c46c030dcdbded7c96b533267f30658deda222
nathan-assefa <nnathanassefa@gmail.com>
2022-09-21 23:03:48 +0300

    More in pointer

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,16 +11,13 @@ char *string_toupper(char *s)
 {
     int i = 0;
 
-    for(i = 0; s[i] != '\0'; i++)
+    while (s[i])
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
+        if (s[i] >= 97 && s[i] <= 122)
         {
             s[i] = s[i] - 32;
         }
-        else
-        {
-            s[i];
-        }
+        i++;
     }
     return (s);
 }
