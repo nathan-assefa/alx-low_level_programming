4d21c5c3b2f38db9ac015d3be6c622591562e1a5
nathan-assefa <nnathanassefa@gmail.com>
2022-09-22 23:21:47 +0300

    Encription

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 char transform_2(char x);
 
@@ -9,73 +9,41 @@ char transform_2(char x);
  */
 char *rot13(char *src)
 {
-	int i = 0;
+int i = 0;
 
-	while (src[i] != '\0')
-	{
-		src[i] = transform_2(src[i]);
-		i++;
-	}
-	return (src);
+while (src[i] != '\0')
+{
+src[i] = transform_2(src[i]);
+i++;
+}
+return (src);
 }
 
 char transform_2(char x)
 {
-	char one[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-	'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-	'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-	's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char two[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-	'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-	'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-	'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-	'i', 'j', 'k', 'l', 'm'};
+char one[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+'s', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char two[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm'};
 
-	int i = 0;
-	char replacment = x;
+int i = 0;
+char replacment = x;
 
-	while (i < 52)
-	{
-		if (x == one[i])
-		{
-			replacment = two[i];
-			break;
-		}
-		i++;
-
-	}
-	return (replacment);
-}
-
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+while (i < 52)
 {
-    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
-    char *p;
-
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    return (0);
+if (x == one[i])
+{
+replacment = two[i];
+break;
 }
+i++;
 
-
-
+}
+return (replacment);
+}
