072b9f362939e0089ad144892f5d7592bb8e162f
nathan-assefa <nnathanassefa@gmail.com>
2022-09-22 03:46:48 -0700

    Encription

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rot13- Encrpting information
+ * @src: parameter
+ * Return: adress
+ */
+char *rot13(const char *src)
+{
+int index1 = 0, index2;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+	'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+	'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+	's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+	'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+	'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+	'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+	'i', 'j', 'k', 'l', 'm'};
+
+while (src[index1])
+{
+	for (index2 = 0; index2 < 52; index2++)
+	{
+		if (src[index1] == alphabet[index2])
+		{
+			src[index1] = rot13key[index2];
+			break;
+		}
+	}
+	index1++;
+}
+return (src);
+}
