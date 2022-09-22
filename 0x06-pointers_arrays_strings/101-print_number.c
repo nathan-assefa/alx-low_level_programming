4ed1b89b7b147e59a5b7a3fd09d95fcc8eb30423
nathan-assefa <nnathanassefa@gmail.com>
2022-09-23 00:01:44 +0300

    Printng number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,15 @@ void print_number(int n);
  */
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
+	unsigned int num = n;
 
-	if (n / 10)
-		print_number(n / 10);
+        if (n < 0)
+        {
+                _putchar('-');
+                num = -num;
+        }
 
-	_putchar(n % 10 + '0');
+        if ((num / 10) > 0)
+                print_number(n / 10);
+        _putchar((num % 10) + '0');
 }
