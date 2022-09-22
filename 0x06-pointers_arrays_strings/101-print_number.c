5395b5c156d4bb291fa89f6f9923b1054f8ada82
nathan-assefa <nnathanassefa@gmail.com>
2022-09-22 18:19:43 +0300

    Removing space

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number- Printing number
+ * @n: parameter
+ */
+void print_number(int n)
+{
+        if (n < 0)
+        {
+                putchar('-');
+                n = -n;
+        }
+
+        if (n / 10)
+                print_number(n / 10);
+
+        putchar(n % 10 + '0');
+}
