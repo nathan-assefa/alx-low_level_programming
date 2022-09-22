5b82120dc1320f71f5140ed80fa0f5a1dc438921
nathan-assefa <nnathanassefa@gmail.com>
2022-09-22 05:16:58 -0700

    Kind of recursion

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number- Printing number
+ * @n: parameter
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = num;
+	}
+
+	if ((n / 10) > 0)
+	{
+		print_nubmber(num / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
