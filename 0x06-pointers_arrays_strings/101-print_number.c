8bef6568781b558cdbce34d0aeac890632e7a26a
nathan-assefa <nnathanassefa@gmail.com>
2022-09-22 23:48:12 +0300

    Printing number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,28 +7,14 @@ void print_number(int n);
  */
 void print_number(int n)
 {
-<<<<<<< HEAD
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 
 	if (n / 10)
 		print_number(n / 10);
 
-	putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
-=======
-        if (n < 0)
-        {
-                putchar('-');
-                n = -n;
-        }
-
-        if (n / 10)
-                print_number(n / 10);
-
-        putchar(n % 10 + '0');
-}
->>>>>>> 55c35b6e10d196d79303781e15579fae211f63b4
