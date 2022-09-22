2cd3d546a08c2a604cb7ab88268630afab07cdbd
nathan-assefa <nnathanassefa@gmail.com>
2022-09-22 23:17:10 +0300

    Encription

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,35 +1,81 @@
-#include "main.h"
+#include <stdio.h>
+
+char transform_2(char x);
 
 /**
  * rot13- Encrpting information
  * @src: parameter
  * Return: adress
  */
-char *rot13(const char *src)
+char *rot13(char *src)
 {
-int index1 = 0, index2;
-char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		src[i] = transform_2(src[i]);
+		i++;
+	}
+	return (src);
+}
+
+char transform_2(char x)
+{
+	char one[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 	'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
 	'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
 	'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 	's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+	char two[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
 	'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 	'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
 	'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 	'i', 'j', 'k', 'l', 'm'};
 
-while (src[index1])
-{
-	for (index2 = 0; index2 < 52; index2++)
+	int i = 0;
+	char replacment = x;
+
+	while (i < 52)
 	{
-		if (src[index1] == alphabet[index2])
+		if (x == one[i])
 		{
-			src[index1] = rot13key[index2];
+			replacment = two[i];
 			break;
 		}
+		i++;
+
 	}
-	index1++;
+	return (replacment);
 }
-return (src);
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+    char *p;
+
+    p = rot13(s);
+    printf("%s", p);
+    printf("------------------------------------\n");
+    printf("%s", s);
+    printf("------------------------------------\n");
+    p = rot13(s);
+    printf("%s", p);
+    printf("------------------------------------\n");
+    printf("%s", s);
+    printf("------------------------------------\n");
+    p = rot13(s);
+    printf("%s", p);
+    printf("------------------------------------\n");
+    printf("%s", s);
+    return (0);
 }
+
+
+
