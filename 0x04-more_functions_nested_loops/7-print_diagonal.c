66053eb5af7a08f560531f8aefd99018994ff08a
nathan-assefa <nnathanassefa@gmail.com>
2022-09-24 10:39:47 +0300

    Making diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,20 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_diagonal- Printig diagonal
  *
  * @n: parameter
  */
-
-int main(void)
-{
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
-    return (0);
-}
 void print_diagonal(int n)
 {
 if (n <= 0)
