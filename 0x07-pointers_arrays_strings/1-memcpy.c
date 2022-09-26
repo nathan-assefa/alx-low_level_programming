03b401c1569eb176c01b150f5f60e5dca94cc80f
nathan-assefa <nnathanassefa@gmail.com>
2022-09-26 07:12:19 -0700

    Copying a memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * -memcpy - copyies a memory area
- * @dest: pointer
- * @src: pointer
- * @n: parameter
+ * _memcpy - copying a memory
+ *  @dest: pointer
+ *  @src: pointer
+ *  @n: parameter
  *
- * Return: always dest
+ *  Return: always dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
+	unsigned int i = 0;
 
-for (; i <n; i++)
-{
-dest[i] = src[i];
-}
-return (dest);
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
