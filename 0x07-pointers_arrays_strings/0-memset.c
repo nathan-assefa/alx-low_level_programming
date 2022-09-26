3c39bd5a9aa2f518880716c714177c2860861687
nathan-assefa <nnathanassefa@gmail.com>
2022-09-26 06:38:58 -0700

    Adding comment

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,11 +7,7 @@
  * @b: parameter
  * @n: parameter
  *
-<<<<<<< HEAD
  * Return: pointer to the memory bolck
-=======
- * Return: always a charachter
->>>>>>> 0fa9857011f2f82f7ff58dbd9bc00dc26ff40620
  */
 char *_memset(char *s, char b, unsigned int n)
 {
