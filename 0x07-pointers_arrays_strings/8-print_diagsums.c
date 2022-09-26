2527b93c8c35a8bcaea6c22303d781fe5bb20caf
nathan-assefa <nnathanassefa@gmail.com>
2022-09-26 13:15:01 -0700

    Summing diagonals of matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,18 +7,33 @@
  * @size: width of matrix columun
  */
 void print_diagsums(int *a, int size)
-{
-    int i, j, p, l = 0, r = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        p = (i * size) + 1;
-        l += *(a + p);
-    }
-    for (j = 0; j < size; j++)
-    {
-        p = (j * size) + (size - 1 + i);
-        r += *(a + p);
-    }
-    printf("%i, %i\n", l, r);
+{
+
+	int i, j, p, l = 0, r = 0;
+
+
+
+	for (i = 0; i < size; i++)
+
+	{
+
+		p = (i * size) + 1;
+
+		l += *(a + p);
+
+	}
+
+	for (j = 0; j < size; j++)
+
+	{
+
+		p = (j * size) + (size - 1 + i);
+
+		r += *(a + p);
+
+	}
+
+	printf("%i, %i\n", l, r);
+
 }
