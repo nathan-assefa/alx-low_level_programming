93459a29bbf8e804bdc1fed666cbe11136f272e4
nathan-assefa <nnathanassefa@gmail.com>
2022-09-28 22:55:30 +0300

    Revesing a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,10 +11,10 @@ void _puts_recursion(char *s)
 
     if (s[i] == '\0')
     {
-        putchar('\n');
+        _putchar('\n');
         return;
     }
-    putchar(s[i]);
+    _putchar(s[i]);
     i++;
     _puts_recursion(s + i);
 }
