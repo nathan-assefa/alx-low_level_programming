21db20bd8261e56f61812401b9c44f1b31966878
nathan-assefa <nnathanassefa@gmail.com>
2022-09-29 00:48:19 +0300

    Measurng length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,11 @@
  */
 int _strlen_recursion(char *s)
 {
-    int len = 0;
+    if (*s == '\0')
+    return (0);
 
-    if (s[len])
+    else
     {
-        len++;
-        len += _strlen_recursion(s + 1);
+        return (1 + _strlen_recursion(s + 1));
     }
 }
