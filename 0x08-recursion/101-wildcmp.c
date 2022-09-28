96b40c4e72d360ada4a86e064cf876df5845a797
nathan-assefa <nnathanassefa@gmail.com>
2022-09-28 15:55:16 -0700

    Adding comment

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,58 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * * wildcmp - compares two strings and returns 1 if identical
+ * * @s1: string to be checked
+ * * @s2: pattern to be used
+ * *
+ * * Return: 1 if identical, 0 otherwise
  */
-int main(void)
-{
-    int r;
-
-    r = wildcmp("main.c", "*.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*a*i*n*.*c*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "main.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "ma********************************c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "***");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m.*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "**.*c");
-    printf("%d\n", r);
-    r = wildcmp("main-main.c", "ma*in.c");
-    printf("%d\n", r);
-    r = wildcmp("main", "main*d");
-    printf("%d\n", r);
-    r = wildcmp("abc", "*b");
-    printf("%d\n", r);
-    return (0);
-}
-
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0')
-    {
-        if (*s2 != '\0' && *s2 == '*')
-        return (wildcmp(s1, s2 + 1));
-        return (*s2 == '\0');
-
-    }
-
-    if (*s2 == '\0')
-    return (wildcmp(s1 + 1, s2));
-
-    else if (*s1 == *s2)
-    {
-        return (wildcmp(s1 + 1, s2 + 1));
-    }
-    return (0);
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
 }
