28962caa3e9731b71e39f9c0d328baffcb7940df
nathan-assefa <nnathanassefa@gmail.com>
2022-10-01 00:41:24 +0300

    Finding the least coin

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the least amout of coin
+ *
+ * @argc: argument count
+ * @argv: argument list
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+int total, cents[] = {25, 10, 5, 2, 1}, coins = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+total = atoi(argv[1]);
+
+while (total > 0)
+{
+if(total >= cents[0])
+total -= cents[0];
+else if (total >= cents[1])
+total -= cents[1];
+else if (total >= cents[2])
+total -= cents[2];
+else if (total >= cents[3])
+total -= cents[3];
+else if (total >= cents[4])
+total -= cents[4];
+
+coins++;
+}
+printf("%d", coins);
+}
+return (0);
+}
