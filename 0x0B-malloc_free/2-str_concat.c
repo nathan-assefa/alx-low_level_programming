4039f79879465da77af048c0d706381a185e439a
nathan-assefa <nnathanassefa@gmail.com>
2022-10-04 06:45:31 -0700

    Adding space

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned long int i, j;
 	char *concat;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	concat = malloc(strlen(s1) + strlen(s2) + 1);
 
