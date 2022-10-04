876e70a148b2eec855657d32c0194596637e331c
nathan-assefa <nnathanassefa@gmail.com>
2022-10-04 01:18:13 -0700

    Concatinating string

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,36 +6,28 @@
  * @ac: parameter
  * @av: double pointer
  * Return: pointer
-
  */
 char *argstostr(int ac, char **av)
 {
 	int i, j, k, size;
 	char *arg;
 
-	size = 0;
-	k = 0;
+	size = 0, k = 0;
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
-
 	i = 0;
 	while (i < ac)
 	{
 		j = 0;
 		while (av[i][j])
 		{
-			size++;
-			j++;
+			size++, j++;
 		}
-		size++;
-		i++;
+		size++, i++;
 	}
 	arg = malloc((sizeof(char) * size) + 1);
 	if (arg == NULL)
 		return (NULL);
-
 	i = 0;
 	while (i < ac)
 	{
@@ -43,14 +35,11 @@ char *argstostr(int ac, char **av)
 		while (av[i][j])
 		{
 			arg[k] = av[i][j];
-			j++;
-			k++;
+			j++, k++;
 		}
 		arg[k] = '\n';
-		k++;
-		i++;
+		k++, i++;
 	}
 	arg[k] = '\0';
 	return (arg);
-
 }
