7579b1b863fbbb2c300026ed84200d2f899e72f2
nathan-assefa <nnathanassefa@gmail.com>
2022-10-04 05:38:49 -0700

    Adding comment

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *s
 
 	if (size == 0)
 		return (0);
 
-	char *s = malloc(sizeof(char) * size);
+	*s = malloc(sizeof(char) * size);
 
 	if (s == NULL)
 		return (NULL);
