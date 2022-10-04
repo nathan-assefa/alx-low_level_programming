772ec75430298ee13b1338696fa53364dbfc46f5
nathan-assefa <nnathanassefa@gmail.com>
2022-10-04 06:28:01 -0700

    Adding space

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,6 +24,12 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(grid[i];
+			}
+			free(grid);
 			return (NULL);
 	}
 
