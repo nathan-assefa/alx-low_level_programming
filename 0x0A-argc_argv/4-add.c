41ba981a7241b98b9333d6e0a8e0495dc0c9ecd6
nathan-assefa <nnathanassefa@gmail.com>
2022-10-04 08:17:08 -0700

    Adding line

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,55 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- * main - entry point
- * @argc: argument
- * @argv: argument
- * Return: 0
+ * * check_num- return int
+ * * @str: pointer
+ * * Return: int
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * * main- entery point
+ * * @argc: argument count
+ * * @argv: argument vector
+ * * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-int sum = 0, i;
+	int count, s, sum = 0;
 
-if (argc > 1)
-{
-for (i = 1; i < argc; i++)
-{
-int b;
-char *str;
-
-str = argv[i];
-for (b = 0; b < str[b]; b++)
-{
-if (str[b] < 48 || str[b] > 57)
-{
-printf("Error\n");
-return (1);
-}
-
-}
-}
-}
-for (i = 1; i <= argc; i++)
-{
-sum += atoi(argv[i]);
-}
-printf("%d", sum);
-return(0);
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			s = atoi(argv[count]);
+			sum += s;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
