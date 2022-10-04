a013996b8202fb012a5c82149fc91372d0a361de
nathan-assefa <nnathanassefa@gmail.com>
2022-10-04 05:56:17 -0700

    xxx

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,11 +11,11 @@
  */
 int **alloc_grid(int width, int height)
 {
-	if (width == 0 || height == 0)
-		return (NULL);
-
 	int **matrix;
 	int i, j;
+
+	if (width == 0 || height == 0)
+		return (NULL);
 
 	matrix = malloc(sizeof(int *) * height);
 	if (matrix == NULL)
