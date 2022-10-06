26bdfd53d28766981dee4592c247c37a4a9ff5d8
nathan-assefa <nnathanassefa@gmail.com>
2022-10-06 22:39:32 +0300

    Range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,50 +1,5 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
-int *array_range(int min, int max);
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(int *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int *a;
-
-    a = array_range(0, 10);
-    simple_print_buffer(a, 11);
-    free(a);
-    return (0);
-}
 
 /**
  * array_range - printing ranges of number
@@ -54,23 +9,21 @@ int main(void)
  */
 int *array_range(int min, int max)
 {
-    int i;
-    int *range;
+int *arr, i = 0, t = min;
 
-    if (min > max)
-    {
-        return (NULL);
-    }
+if (min > max)
+{
+return (0);
+}
 
-    range = malloc(sizeof(int) * max);
-    if (range == NULL)
-    return NULL;
-
-    for (i = 0; i <= max; i++)
-    {
-        range[i] = i;
-    }
-    return (range);
-
-
+arr = malloc((max - min + 1) * sizeof(int));
+if (!arr)
+{
+return (0);
+}
+while (i <= max - min)
+{
+arr[i++] = t++;
+}
+return (arr);
 }
