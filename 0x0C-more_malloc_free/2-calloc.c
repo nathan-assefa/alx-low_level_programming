67e6e6334243731c958d6690492c9767ca7f5966
nathan-assefa <nnathanassefa@gmail.com>
2022-10-06 03:14:24 -0700

    Fixing style

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 #include <stdlib.h>
 
 /**
@@ -11,17 +12,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    int *byte;
+	int *byte;
 
-    if (nmemb == 0 || size == 0)
-    {
-        return (NULL);
-    }
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
 
-    byte = calloc(nmemb, size);
-    if (byte == NULL)
-    {
-        return (NULL);
-    }
-    return (byte);
+	byte = calloc(nmemb, size);
+	if (byte == NULL)
+	{
+		return (NULL);
+	}
+	return (byte);
 }
