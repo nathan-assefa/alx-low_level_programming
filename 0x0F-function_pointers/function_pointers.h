64be940759c712af6e707f92034a0154bbcd37f5
nathan-assefa <nnathanassefa@gmail.com>
2022-10-12 11:56:53 -0700

    Adding commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,21 @@
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
+#ifndef FUNCTION_POINTERS_H
 
-int _putchar(char c);
+#define FUNCTION_POINTERS_H
+
+
+
+#include <stdlib.h>
+
+
+
 void print_name(char *name, void (*f)(char *));
+
 void array_iterator(int *array, size_t size, void (*action)(int));
+
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
 
 
-#endif /* FUNCTION_POINTER_H */
+
+
+
+#endif
