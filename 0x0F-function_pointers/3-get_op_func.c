4777b8ed3b4e6d5cec33ece2fef066dd175078f7
nathan-assefa <nnathanassefa@gmail.com>
2022-10-12 12:12:06 -0700

    Adding commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,12 @@
 #include "3-calc.h"
 
 /**
- * * get_op_func - function pointer that selects the correct function to perform
+ * * get_op_func - function pointer that
+ * selects the correct function to perform
  * * the operation asked by the user
  * * @s: the operator given by the user
  * *
  * * Return: pointer to the function that corresponds to the
- * * operator given as a parameter
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -28,6 +28,5 @@ int (*get_op_func(char *s))(int, int)
 			return (ops[i].f);
 		i++;
 	}
-
 	return (NULL);
 }
