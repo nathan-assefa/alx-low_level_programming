f44ba35b3e828e87ebff6ac9a8e532b10bab4883
nathan-assefa <nnathanassefa@gmail.com>
2022-10-13 18:09:10 +0300

    Summing

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -23,7 +23,7 @@ int sum_them_all(const unsigned int n, ...)
 
     for (i = 0; i < n; i++)
     {
-        sum += va_arg(arg, int);
+        sum += va_arg(arg, unsigned int);
     }
     va_end(arg);
     return (sum);
