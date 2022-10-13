addb0e000fd8338bd7ee0b2601821d4cceb38144
nathan-assefa <nnathanassefa@gmail.com>
2022-10-13 13:15:34 -0700

    adding line

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,22 +11,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list arg;
-    unsigned int i;
-    int sum = 0;
+	va_list arg;
+	unsigned int i;
+	int sum = 0;
 
-    if (n == 0)
-    {
-        return (0);
-    }
+	if (n == 0)
+	{
+		return (0);
+	}
 
-    va_start(arg, n);
+	va_start(arg, n);
 
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(arg, int);
-    }
-    va_end(arg);
-    return (sum);
-
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg, int);
+	}
+	va_end(arg);
+	return (sum);
 }
