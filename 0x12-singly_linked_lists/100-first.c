81d6e09ea2cd594bb0496c98737a4148e5c0b784
nathan-assefa <nnathanassefa@gmail.com>
2022-10-20 16:50:15 -0700

    Printing before mani

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * before_main - printing before main
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"Ibore my house upon my back!\n");
+}
