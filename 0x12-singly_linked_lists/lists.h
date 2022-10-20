3aa1ef9d5472dcd7f5c45bb4b1b4d7ca184e7dad
nathan-assefa <nnathanassefa@gmail.com>
2022-10-20 16:53:32 -0700

    Printing before mani

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct list_s
 
 int _putchar(char c);
 void before_main(void) __attribute__((constructor));
+void before_main(void);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
