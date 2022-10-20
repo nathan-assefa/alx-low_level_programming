31066e53d404249133d8b98eee95ab56034b42b6
nathan-assefa <nnathanassefa@gmail.com>
2022-10-20 13:24:45 -0700

    Printing linked list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,23 +10,25 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	if (h == NULL)
-		return (NULL);
 
 	i = 0;
 
-	if (h->str == NULL)
+	while (h)
 	{
-		printf("[0] (nail)\n");
-		h = h->next;
-		i++;
-	}
+		if (h->str == NULL)
+		{
+			printf("[0] (nail)\n");
+			h = h->next;
+			i++;
+		}
 
-	while (h != NULL)
-	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		i++;
+		else
+		{
+
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			i++;
+		}
 	}
 
 	return (i);
