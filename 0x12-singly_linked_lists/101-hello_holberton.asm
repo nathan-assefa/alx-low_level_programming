da805fb182d0e09abad41f142b90b9a8b1194e69
nathan-assefa <nnathanassefa@gmail.com>
2022-10-21 15:18:30 -0700

    Assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,21 @@
-global _start
+extern printf
 
 section .text
+	global main
 
-_start:
-mov rax, 1        ; write(
-mov rdi, 1        ;   STDOUT_FILENO,
-mov rsi, msg      ;   "Hello, world!\n",
-mov rdx, msglen   ;   sizeof("Hello, world!\n")
-syscall           ; );
+main
+push rbp
 
-mov rax, 60       ; exit(
-mov rdi, 0        ;   EXIT_SUCCESS
-syscall           ; );
+mov rdi,fmt
+mov rsi,msg
+mov rax,0
+call printf
 
-section .rodata
-msg: db "Hello, world!", 10
-msglen: equ $ - msg
+pop rbp
+
+mov rax,0
+ret
+
+section .data
+msg: do "Hello, Holberton", 0
+fmt: do "%s", 10, 0
