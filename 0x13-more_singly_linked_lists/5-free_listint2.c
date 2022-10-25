70f15ba849a053d6bfbe37355e517d4291e41338
nathan-assefa <nnathanassefa@gmail.com>
2022-10-25 02:32:23 -0700

    Freeing memory

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,12 +12,12 @@ void free_listint2(listint_t **head)
 	if (*head == NULL)
 		return;
 
-	while ((*head)->next)
+	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	(*head)->next = NULL;
+	*head = NULL;
 
 }
