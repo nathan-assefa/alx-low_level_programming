92d0e4c7d8f9f24d12dd454ccb195f6cd8f4394a
nathan-assefa <nnathanassefa@gmail.com>
2022-10-25 01:22:22 -0700

    Insertion in a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 		i++;
 	}
-	if (tmp->next == NULL)
+	if (tmp == NULL || tmp->next == NULL)
 	{
 		return (NULL);
 	}
