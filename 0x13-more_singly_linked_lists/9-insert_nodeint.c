9ef5a3ab2676aa4ef5b4f367ef784496a51e4371
nathan-assefa <nnathanassefa@gmail.com>
2022-10-25 00:38:37 -0700

    Inserting node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,9 +15,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	unsigned int i;
 
-	if (*head == NULL)
+	if (idx == 0)
 	{
-		return (NULL);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 
 	new_node = malloc(sizeof(listint_t));
