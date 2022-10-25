5ab68ba914cb05a2e5126bfb35315b1a1ff658a4
nathan-assefa <nnathanassefa@gmail.com>
2022-10-25 01:59:31 -0700

    Insertion in a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp, *new_node
+	listint_t *tmp, *new_node;
 	unsigned int i, len;
 
 	new_node = malloc(sizeof(listint_t));
