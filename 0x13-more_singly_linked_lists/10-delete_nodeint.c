18eef9779481c6c4e776df0f22b2b6c3e636abcf
nathan-assefa <nnathanassefa@gmail.com>
2022-10-26 05:44:14 -0700

    More in linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp2;
 	unsigned int i;
 
-	if (*head == NULL)
+	if (*head == NULL || index > 7)
 	{
 		return (-1);
 	}
