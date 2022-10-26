b9c9b3ba27cba4683b81d392643118f2c350c836
nathan-assefa <nnathanassefa@gmail.com>
2022-10-26 05:54:19 -0700

    More in linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,11 +12,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp2;
 	unsigned int i;
 
-	if (*head == NULL || index > 7)
+	if (*head == NULL || index > listint_len(*head))
 	{
 		return (-1);
 	}
-
 	tmp2 = *head;
 
 	if (index == 0)
@@ -25,9 +24,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(tmp2);
 		return (1);
 	}
-
-
-
 	else
 	{
 		i = 0;
@@ -44,4 +40,22 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	}
 	return (1);
+}
+
+/**
+ * listint_len- Returns length of linked list
+ * @h: Head of linked list
+ * Return: size_t
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len;
+
+	len = 0;
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
