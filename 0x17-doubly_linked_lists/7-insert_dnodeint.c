5c1ace88cb02930d0ed4e1454229a6d2b6190815
nathan-assefa <nnathanassefa@gmail.com>
2022-12-08 10:22:38 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	dlistint_t *tail, *tmp2;
 
+	if (*h == NULL)
+	{
+		return (NULL);
+	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 	{
@@ -19,11 +23,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	new_node->n = n;
-
-	if (*h == NULL)
-	{
-		return (NULL);
-	}
 
 	tail = *h;
 	if (idx == 0)
