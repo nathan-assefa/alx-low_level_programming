fbc179e26372953f56a17a80bf1e47dc02a51985
nathan-assefa <nnathanassefa@gmail.com>
2023-01-06 13:05:15 -0800

    0x1A-hash_tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,7 +15,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (ht->array[i])
 			{
-				if (comma)
+				if (comma++)
 					printf(", ");
 				printf("'%s': '%s'", ht->array[i]->key,
 						ht->array[i]->value);
