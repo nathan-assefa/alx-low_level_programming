7a35903210a890a3654a7f569e1d8660ad08ef7d
nathan-assefa <nnathanassefa@gmail.com>
2023-01-05 22:59:46 -0800

    0x1A-hash_tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * * hash_table_create- Creating hash table
+ * * @size: the size of items whithin the hash table
+ * * Return: The newly creatd hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash;
+
+	new_hash = malloc(sizeof(hash_table_t) * size);
+	if (new_hash == NULL)
+		return (NULL);
+
+	new_hash->size = size;
+	new_hash->array = malloc(sizeof(hash_node_t) * size);
+	if (new_hash->array == NULL)
+		return (NULL);
+
+	return (new_hash);
+}
