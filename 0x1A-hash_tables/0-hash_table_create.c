6d8334b67220eb0dec2033657ed8e2f6ca3dab4d
nathan-assefa <nnathanassefa@gmail.com>
2023-01-06 06:13:58 -0800

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size <= 0)
 		return (NULL);
 
-	new_hash = malloc(sizeof(hash_table_t) * size);
+	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
 		return (NULL);
 
