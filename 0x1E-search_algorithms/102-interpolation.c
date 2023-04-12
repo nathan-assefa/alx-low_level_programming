8f852b63a5bfb55f8cbb0d6782f9fa1f874c7f1b
nathan-assefa <nnathanassefa@gmail.com>
2023-04-12 13:32:03 -0700

    Searching algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,48 +1,49 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - search for a value in a sorted array using
- * interpolation search
- * @array: pointer to the start of the array
- * @size: the size of the array (number of elements)
- * @value: the value to be searched for
- *
- * Return: first index value is found at or -1 if not present or array is null
- */
+* interpolationSearch - searching for a number
+* @arr: array to be searched
+* @lo: lower index in the array
+* @hi: higher index in the array
+* @x: value to be searched
+* Return: value
+*/
+int interpolationSearch(int arr[], int lo, int hi, int x)
+{
+	int pos = 0;
+
+	if (lo <= hi && x >= arr[lo] && x <= arr[hi])
+	{
+
+		pos = lo
+			+ (((double)(hi - lo) / (arr[hi] - arr[lo]))
+			* (x - arr[lo]));
+		printf("Value checked array[%d] = [%d]\n", pos, arr[pos]);
+
+		if (arr[pos] == x)
+			return pos;
+
+		if (arr[pos] < x)
+			return interpolationSearch(arr, pos + 1, hi, x);
+
+		if (arr[pos] > x)
+			return interpolationSearch(arr, lo, pos - 1, x);
+	}
+	printf("Value checked array[%d] is out of range\n", pos);
+	return -1;
+}
+
+/**
+* interpolation_search - interpolation search algorithm
+* @array: array to be searched
+* @size: size of an array
+* @value: value to be searched
+* Return: value
+*/
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos;
-	int low = 0;
-	int high = size - 1;
-
-	if (array == NULL || size == 0)
+	if (!array)
 		return (-1);
 
-	while (low <= high)
-	{
-		pos = low + (((double)(high - low) / (array[high] - array[low]))
-			     * (value - array[low]));
-
-		if (pos > size - 1)
-		{
-			printf("Value checked array[%ld] is out of range\n", pos);
-			return (-1);
-		}
-
-		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
-
-		if (array[pos] == value)
-		{
-			return (pos);
-		}
-		else
-		{
-			if (array[pos] > value)
-				high = pos - 1;
-			else
-				low = pos + 1;
-		}
-	}
-
-	return (-1);
+	return interpolationSearch(array, 0, size - 1, value);
 }
