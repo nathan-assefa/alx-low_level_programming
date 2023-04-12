2f1bbceee5a91e15bccd7fc405271a8d76993098
nathan-assefa <nnathanassefa@gmail.com>
2023-04-12 07:09:06 -0700

    Searching algorithm

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -41,12 +41,13 @@ int bs(int *array, size_t size, int low, int high, int value)
 	{
 		if (mid == 0 || array[mid - 1] < value)
 			return (mid);
+		return (bs(array, size, low, mid, value));
 	}
 
 	if (low < high)
 	{
 		if (value <= array[mid])
-			return (bs(array, size, low, mid, value));
+			return (bs(array, size, low, mid - 1, value));
 
 		return (bs(array, size, mid + 1, high, value));
 	}
