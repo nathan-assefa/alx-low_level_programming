79af270053740848c444112729b5ed437619403e
nathan-assefa <nnathanassefa@gmail.com>
2023-04-13 04:53:01 -0700

    Searching algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,25 +12,23 @@ int interpolationSearch(int arr[], int lo, int hi, int x)
 {
 	int pos = 0;
 
+	pos = lo + (((double)(hi - lo) / (arr[hi] - arr[lo]))
+		* (x - arr[lo]));
+
 	if (lo <= hi && x >= arr[lo] && x <= arr[hi])
 	{
-
-		pos = lo
-			+ (((double)(hi - lo) / (arr[hi] - arr[lo]))
-			* (x - arr[lo]));
 		printf("Value checked array[%d] = [%d]\n", pos, arr[pos]);
 
 		if (arr[pos] == x)
-			return pos;
+			return (pos);
 
 		if (arr[pos] < x)
-			return interpolationSearch(arr, pos + 1, hi, x);
+			return (interpolationSearch(arr, pos + 1, hi, x));
 
-		if (arr[pos] > x)
-			return interpolationSearch(arr, lo, pos - 1, x);
+		return (interpolationSearch(arr, lo, pos - 1, x));
 	}
 	printf("Value checked array[%d] is out of range\n", pos);
-	return -1;
+	return (-1);
 }
 
 /**
@@ -45,5 +43,5 @@ int interpolation_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	return interpolationSearch(array, 0, size - 1, value);
+	return (interpolationSearch(array, 0, size - 1, value));
 }
